72eb335f8abc1b5053f8f081666ad43c7040471e
Joel Baranick <joel.baranick@ensighten.com>
2019-03-11 16:24:14 -0700

    Fix completions location, now requires greadlink

--- rbenv-lazy.plugin.zsh
+++ rbenv-lazy.plugin.zsh
@@ -2,7 +2,7 @@
 if type rbenv &> /dev/null; then
   local RBENV_SHIMS="${RBENV_ROOT:-${HOME}/.rbenv}/shims"
   export PATH="${RBENV_SHIMS}:${PATH}"
-  source $(whence -p rbenv)/../../completions/rbenv.zsh
+  source $(dirname $(greadlink `whence -p rbenv`))/../completions/rbenv.zsh
   function rbenv() {
     unset -f rbenv > /dev/null 2>&1
     eval "$(command rbenv init -)"
