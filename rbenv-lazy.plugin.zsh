60b164fd4050c40c7cde3e37d84c01ad364ee388
Chris Barnes <barnesc@janelia.hhmi.org>
2019-05-03 18:25:36 -0400

    Plugin sets path

--- rbenv-lazy.plugin.zsh
+++ rbenv-lazy.plugin.zsh
@@ -1,11 +1,17 @@
 # Lazy load rbenv
+export RBENV_ROOT="${RBENV_ROOT:=${HOME}/.rbenv}"
+if ! type rbenv > /dev/null && [ -f "${RBENV_ROOT}/bin/rbenv" ]; then
+    export PATH="${RBENV_ROOT}/bin:${PATH}"
+fi
+
 if type rbenv &> /dev/null; then
-  local RBENV_SHIMS="${RBENV_ROOT:-${HOME}/.rbenv}/shims"
+  local RBENV_SHIMS="${RBENV_ROOT}/shims"
   export PATH="${RBENV_SHIMS}:${PATH}"
-  source $(dirname $(greadlink -f `whence -p rbenv`))/../completions/rbenv.zsh
+  source ${RBENV_ROOT}/completions/rbenv.zsh
   function rbenv() {
     unset -f rbenv > /dev/null 2>&1
     eval "$(command rbenv init -)"
     rbenv "$@"
   }
 fi
+
